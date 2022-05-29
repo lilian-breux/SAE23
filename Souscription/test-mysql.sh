20de706bf03b4f8493682b90839029ffc73f9d77
lilian-breux <105793062+lilian-breux@users.noreply.github.com>
2022-05-29 08:22:50 +0200

    Changement de la base de donnée

--- Souscription/test-mysql.sh
+++ Souscription/test-mysql.sh
@@ -7,6 +7,6 @@
 
 #185.176.40.25
 
-query="INSERT INTO \`Mesure\` (\`bate\`, \`room\`, \`type\`, \`date\`, \`hours\`, \`value\`, \`id\`) VALUES ('RT', 'R208', 'co2', '2022-05-26', '2022-05-26 08:32:00', '730', NULL);"
+query="INSERT INTO \`Mesure\` (\`id\`, \`bate\`, \`room\`, \`type\`, \`date\`, \`hours\`, \`value\`) VALUES (NULL, 'RT', 'R208', 'co2', '2022-05-26', '2022-05-26 08:32:00', '730');"
 mysql -h fdb31.eohost.com --port=3306 -u 3953973_lmll -pMatLucLuiLil4 mysql -e "$query";
 

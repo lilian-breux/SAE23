b8dca2211b57574ad09dd7a3e418c594ce928edf
lilian-breux <105793062+lilian-breux@users.noreply.github.com>
2022-06-07 00:27:02 +0200

    Add files via upload

--- Site_web/INFO.php
+++ Site_web/INFO.php
@@ -14,10 +14,178 @@
         <link rel="stylesheet" type="text/css" href="./styles/style.css" />
     </head>
     <body>
+        <h1>Batiment informatique</h1>
         <?php 
             include("./includes/nav.php");
             include("mysql.php");
     	?>
-        <p>Info</p>
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="4">Salle B208</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Temperature</td>
+                    <td>Date</td>
+                    <td>Heure</td>
+                </tr>
+                <?php 
+                        $requete="SELECT `Capteur`.`bate`, `Capteur`.`room`, `Capteur`.`type`, `Valeur`.`value`, `Mesure`.`date`, `Mesure`.`hours`
+                                FROM `Capteur` 
+                                    LEFT JOIN `Valeur` ON `Valeur`.`idcap` = `Capteur`.`idcapt` 
+                                    LEFT JOIN `Mesure` ON `Valeur`.`idmesu` = `Mesure`.`idmesu`
+                                WHERE bate='INFO' AND type='temperature' AND room='B208'
+                                ORDER BY date DESC, hours DESC;";
+                        $resultat = mysqli_query($id_bd, $requete)
+                            or die("Execution de la requete impossible : $requete");
+                        while($ligne=mysqli_fetch_array($resultat)){
+                            echo "<tr><td>".$ligne['value']."</td><td>".$ligne['date']."</td><td>".$ligne['hours']."</td></tr>";
+                        }
+
+                ?>
+            </tbody>
+        </table>
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="4">Salle B208</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Luminosité</td>
+                    <td>Date</td>
+                    <td>Heure</td>
+                </tr>
+                <?php 
+                        $requete="SELECT `Capteur`.`bate`, `Capteur`.`room`, `Capteur`.`type`, `Valeur`.`value`, `Mesure`.`date`, `Mesure`.`hours`
+                                FROM `Capteur` 
+                                    LEFT JOIN `Valeur` ON `Valeur`.`idcap` = `Capteur`.`idcapt` 
+                                    LEFT JOIN `Mesure` ON `Valeur`.`idmesu` = `Mesure`.`idmesu`
+                                WHERE bate='INFO' AND type='luminosite' AND room='B208'
+                                ORDER BY date DESC, hours DESC;";
+                        $resultat = mysqli_query($id_bd, $requete)
+                            or die("Execution de la requete impossible : $requete");
+                        while($ligne=mysqli_fetch_array($resultat)){
+                            echo "<tr><td>".$ligne['value']."</td><td>".$ligne['date']."</td><td>".$ligne['hours']."</td></tr>";
+                        }
+
+                ?>
+            </tbody>
+        </table>
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="4">Salle B208</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Co2</td>
+                    <td>Date</td>
+                    <td>Heure</td>
+                </tr>
+                <?php 
+                        $requete="SELECT `Capteur`.`bate`, `Capteur`.`room`, `Capteur`.`type`, `Valeur`.`value`, `Mesure`.`date`, `Mesure`.`hours`
+                                FROM `Capteur` 
+                                    LEFT JOIN `Valeur` ON `Valeur`.`idcap` = `Capteur`.`idcapt` 
+                                    LEFT JOIN `Mesure` ON `Valeur`.`idmesu` = `Mesure`.`idmesu`
+                                WHERE bate='INFO' AND type='co2' AND room='B208'
+                                ORDER BY date DESC, hours DESC;";
+                        $resultat = mysqli_query($id_bd, $requete)
+                            or die("Execution de la requete impossible : $requete");
+                        while($ligne=mysqli_fetch_array($resultat)){
+                            echo "<tr><td>".$ligne['value']."</td><td>".$ligne['date']."</td><td>".$ligne['hours']."</td></tr>";
+                        }
+
+                ?>
+            </tbody>
+        </table>
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="4">Salle B207</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Temperature</td>
+                    <td>Date</td>
+                    <td>Heure</td>
+                </tr>
+                <?php 
+                        $requete="SELECT `Capteur`.`bate`, `Capteur`.`room`, `Capteur`.`type`, `Valeur`.`value`, `Mesure`.`date`, `Mesure`.`hours`
+                                FROM `Capteur` 
+                                    LEFT JOIN `Valeur` ON `Valeur`.`idcap` = `Capteur`.`idcapt` 
+                                    LEFT JOIN `Mesure` ON `Valeur`.`idmesu` = `Mesure`.`idmesu`
+                                WHERE bate='INFO' AND type='temperature' AND room='B207'
+                                ORDER BY date DESC, hours DESC;";
+                        $resultat = mysqli_query($id_bd, $requete)
+                            or die("Execution de la requete impossible : $requete");
+                        while($ligne=mysqli_fetch_array($resultat)){
+                            echo "<tr><td>".$ligne['value']."</td><td>".$ligne['date']."</td><td>".$ligne['hours']."</td></tr>";
+                        }
+
+                ?>
+            </tbody>
+        </table>
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="4">Salle B207</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Luminosité</td>
+                    <td>Date</td>
+                    <td>Heure</td>
+                </tr>
+                <?php 
+                        $requete="SELECT `Capteur`.`bate`, `Capteur`.`room`, `Capteur`.`type`, `Valeur`.`value`, `Mesure`.`date`, `Mesure`.`hours`
+                                FROM `Capteur` 
+                                    LEFT JOIN `Valeur` ON `Valeur`.`idcap` = `Capteur`.`idcapt` 
+                                    LEFT JOIN `Mesure` ON `Valeur`.`idmesu` = `Mesure`.`idmesu`
+                                WHERE bate='INFO' AND type='luminosite' AND room='B207'
+                                ORDER BY date DESC, hours DESC;";
+                        $resultat = mysqli_query($id_bd, $requete)
+                            or die("Execution de la requete impossible : $requete");
+                        while($ligne=mysqli_fetch_array($resultat)){
+                            echo "<tr><td>".$ligne['value']."</td><td>".$ligne['date']."</td><td>".$ligne['hours']."</td></tr>";
+                        }
+
+                ?>
+            </tbody>
+        </table>
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="4">Salle B207</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Co2</td>
+                    <td>Date</td>
+                    <td>Heure</td>
+                </tr>
+                <?php 
+                        $requete="SELECT `Capteur`.`bate`, `Capteur`.`room`, `Capteur`.`type`, `Valeur`.`value`, `Mesure`.`date`, `Mesure`.`hours`
+                                FROM `Capteur` 
+                                    LEFT JOIN `Valeur` ON `Valeur`.`idcap` = `Capteur`.`idcapt` 
+                                    LEFT JOIN `Mesure` ON `Valeur`.`idmesu` = `Mesure`.`idmesu`
+                                WHERE bate='INFO' AND type='co2' AND room='B207'
+                                ORDER BY date DESC, hours DESC;";
+                        $resultat = mysqli_query($id_bd, $requete)
+                            or die("Execution de la requete impossible : $requete");
+                        while($ligne=mysqli_fetch_array($resultat)){
+                            echo "<tr><td>".$ligne['value']."</td><td>".$ligne['date']."</td><td>".$ligne['hours']."</td></tr>";
+                        }
+
+                ?>
+            </tbody>
+        </table>
     </body>
 </html>
--- Site_web/RT.php
+++ Site_web/RT.php
@@ -14,10 +14,178 @@
         <link rel="stylesheet" type="text/css" href="./styles/style.css" />
     </head>
     <body>
+        <h1>Batiment réseau</h1>
         <?php 
             include("./includes/nav.php");
             include("mysql.php");
     	?>
-        <p>RT</p>
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="4">Salle E208</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Temperature</td>
+                    <td>Date</td>
+                    <td>Heure</td>
+                </tr>
+                <?php 
+                        $requete="SELECT `Capteur`.`bate`, `Capteur`.`room`, `Capteur`.`type`, `Valeur`.`value`, `Mesure`.`date`, `Mesure`.`hours`
+                                FROM `Capteur` 
+                                    LEFT JOIN `Valeur` ON `Valeur`.`idcap` = `Capteur`.`idcapt` 
+                                    LEFT JOIN `Mesure` ON `Valeur`.`idmesu` = `Mesure`.`idmesu`
+                                WHERE bate='RT' AND type='temperature' AND room='E208'
+                                ORDER BY date DESC, hours DESC;";
+                        $resultat = mysqli_query($id_bd, $requete)
+                            or die("Execution de la requete impossible : $requete");
+                        while($ligne=mysqli_fetch_array($resultat)){
+                            echo "<tr><td>".$ligne['value']."</td><td>".$ligne['date']."</td><td>".$ligne['hours']."</td></tr>";
+                        }
+
+                ?>
+            </tbody>
+        </table>
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="4">Salle E208</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Luminosité</td>
+                    <td>Date</td>
+                    <td>Heure</td>
+                </tr>
+                <?php 
+                        $requete="SELECT `Capteur`.`bate`, `Capteur`.`room`, `Capteur`.`type`, `Valeur`.`value`, `Mesure`.`date`, `Mesure`.`hours`
+                                FROM `Capteur` 
+                                    LEFT JOIN `Valeur` ON `Valeur`.`idcap` = `Capteur`.`idcapt` 
+                                    LEFT JOIN `Mesure` ON `Valeur`.`idmesu` = `Mesure`.`idmesu`
+                                WHERE bate='RT' AND type='luminosite' AND room='E208'
+                                ORDER BY date DESC, hours DESC;";
+                        $resultat = mysqli_query($id_bd, $requete)
+                            or die("Execution de la requete impossible : $requete");
+                        while($ligne=mysqli_fetch_array($resultat)){
+                            echo "<tr><td>".$ligne['value']."</td><td>".$ligne['date']."</td><td>".$ligne['hours']."</td></tr>";
+                        }
+
+                ?>
+            </tbody>
+        </table>
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="4">Salle E208</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Co2</td>
+                    <td>Date</td>
+                    <td>Heure</td>
+                </tr>
+                <?php 
+                        $requete="SELECT `Capteur`.`bate`, `Capteur`.`room`, `Capteur`.`type`, `Valeur`.`value`, `Mesure`.`date`, `Mesure`.`hours`
+                                FROM `Capteur` 
+                                    LEFT JOIN `Valeur` ON `Valeur`.`idcap` = `Capteur`.`idcapt` 
+                                    LEFT JOIN `Mesure` ON `Valeur`.`idmesu` = `Mesure`.`idmesu`
+                                WHERE bate='RT' AND type='co2' AND room='E208'
+                                ORDER BY date DESC, hours DESC;";
+                        $resultat = mysqli_query($id_bd, $requete)
+                            or die("Execution de la requete impossible : $requete");
+                        while($ligne=mysqli_fetch_array($resultat)){
+                            echo "<tr><td>".$ligne['value']."</td><td>".$ligne['date']."</td><td>".$ligne['hours']."</td></tr>";
+                        }
+
+                ?>
+            </tbody>
+        </table>
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="4">Salle E207</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Temperature</td>
+                    <td>Date</td>
+                    <td>Heure</td>
+                </tr>
+                <?php 
+                        $requete="SELECT `Capteur`.`bate`, `Capteur`.`room`, `Capteur`.`type`, `Valeur`.`value`, `Mesure`.`date`, `Mesure`.`hours`
+                                FROM `Capteur` 
+                                    LEFT JOIN `Valeur` ON `Valeur`.`idcap` = `Capteur`.`idcapt` 
+                                    LEFT JOIN `Mesure` ON `Valeur`.`idmesu` = `Mesure`.`idmesu`
+                                WHERE bate='RT' AND type='temperature' AND room='E207'
+                                ORDER BY date DESC, hours DESC;";
+                        $resultat = mysqli_query($id_bd, $requete)
+                            or die("Execution de la requete impossible : $requete");
+                        while($ligne=mysqli_fetch_array($resultat)){
+                            echo "<tr><td>".$ligne['value']."</td><td>".$ligne['date']."</td><td>".$ligne['hours']."</td></tr>";
+                        }
+
+                ?>
+            </tbody>
+        </table>
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="4">Salle E207</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Luminosité</td>
+                    <td>Date</td>
+                    <td>Heure</td>
+                </tr>
+                <?php 
+                        $requete="SELECT `Capteur`.`bate`, `Capteur`.`room`, `Capteur`.`type`, `Valeur`.`value`, `Mesure`.`date`, `Mesure`.`hours`
+                                FROM `Capteur` 
+                                    LEFT JOIN `Valeur` ON `Valeur`.`idcap` = `Capteur`.`idcapt` 
+                                    LEFT JOIN `Mesure` ON `Valeur`.`idmesu` = `Mesure`.`idmesu`
+                                WHERE bate='RT' AND type='luminosite' AND room='E207'
+                                ORDER BY date DESC, hours DESC;";
+                        $resultat = mysqli_query($id_bd, $requete)
+                            or die("Execution de la requete impossible : $requete");
+                        while($ligne=mysqli_fetch_array($resultat)){
+                            echo "<tr><td>".$ligne['value']."</td><td>".$ligne['date']."</td><td>".$ligne['hours']."</td></tr>";
+                        }
+
+                ?>
+            </tbody>
+        </table>
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="4">Salle E207</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Co2</td>
+                    <td>Date</td>
+                    <td>Heure</td>
+                </tr>
+                <?php 
+                        $requete="SELECT `Capteur`.`bate`, `Capteur`.`room`, `Capteur`.`type`, `Valeur`.`value`, `Mesure`.`date`, `Mesure`.`hours`
+                                FROM `Capteur` 
+                                    LEFT JOIN `Valeur` ON `Valeur`.`idcap` = `Capteur`.`idcapt` 
+                                    LEFT JOIN `Mesure` ON `Valeur`.`idmesu` = `Mesure`.`idmesu`
+                                WHERE bate='RT' AND type='co2' AND room='E207'
+                                ORDER BY date DESC, hours DESC;";
+                        $resultat = mysqli_query($id_bd, $requete)
+                            or die("Execution de la requete impossible : $requete");
+                        while($ligne=mysqli_fetch_array($resultat)){
+                            echo "<tr><td>".$ligne['value']."</td><td>".$ligne['date']."</td><td>".$ligne['hours']."</td></tr>";
+                        }
+
+                ?>
+            </tbody>
+        </table>
     </body>
 </html>

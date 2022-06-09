412cd6646ba17ec1f160321d6b42d6fc99196fad
IUTLuisGP <105793515+IUTLuisGP@users.noreply.github.com>
2022-06-09 09:35:34 +0200

    Update index.php

--- Site_web/index.php
+++ Site_web/index.php
@@ -1,25 +1,32 @@
 <?php 
 	session_start();
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
  <head>
+  /* Title of the website */
   <title>SAE 23</title>
+	 
+  /* Definition of the metadata of the website */
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1" /> 
   <meta http-equiv= "X-UA-Compatible" content= "IE=edge" />
   <meta name="description" content="SAE" />
   <meta name="keywords" content="HTML, CSS" />
-  <link rel="stylesheet" type="text/css" href="./styles/style.css" />
+  <link rel="stylesheet" type="text/css" href="../styles/style.css" />
  </head>
+	
+	/* Beginning of the website's body */
 	 <body>
 	 	<?php 
             include("./includes/nav.php");
-    	?>
+    		?>
+		 /* Titre */
 		<h1> SAE 23 - Mettre en place une solution informatique pour l'entreprise </h1>
-
 		<br>
-
+		 
+		 /* Beginning of the PHP code */ 
 		<?php 
 			include ("mysql.php");
 			
@@ -27,7 +34,8 @@
 			$requete = "SELECT count(*) FROM `Administration`";
 			$exec_requete = mysqli_query($id_bd,$requete);
 			$reponse = mysqli_fetch_array($exec_requete);
-
+			
+		 	/* Creation of the form allowing a user to login */ 
 			$count = $reponse['count(*)'];
 			if($count==0){
 				echo "<h1>Création de compte administrateur</h1>";
@@ -39,12 +47,16 @@
 				echo "</form>";
 			}
 		?>
+		 
+		 /* Saé's presentation paragraph */
 		<p> L'objectif de ce site web est d'offrir aux gestionnaires des bâtiments de l'IUT de Blagnac
 			une interface conviviale et simple pour la visualisation des données capteurs. </p>
 		<br>
 		<h1> Bâtiments gérés </h1>
 		<p> Voici la liste des bâtiments gérés avec leurs salles, où on retroue des capteurs de luminosité,
-			CO2 et température: </p> 
+		    CO2 et température: </p>
+		 
+		 /* Listing the building halls where there's a sensor */ 
 		<ul>
 			<li> Bâtiment B </li>
 				<ul>
@@ -59,10 +71,12 @@
 				</ul>
 		</ul>
 		<br>
-	 </body>
+		 
+	/* Website's footer */
 	 <footer>
 		<ul>
 			<li><a href="mlegales.html"> Mentions Légales </a></li>
 		</ul>
-	 </footer>
+	 </footer> 
+	</body>
 </html>

c6afe5a8fb053ebc436ebf6360a52789aa9f23bb
IUTLuisGP <105793515+IUTLuisGP@users.noreply.github.com>
2022-06-09 11:40:37 +0200

    coorections Commentaires

--- Site_web/index.php
+++ Site_web/index.php
@@ -5,10 +5,10 @@
 <!DOCTYPE html>
 <html lang="fr">
  <head>
-  /* Title of the web page */
+  <!-- Title of the web page -->
   <title>SAE 23</title>
 	 
-  /* Definition of the metadata of the website */
+  <!-- Definition of the metadata of the website -->
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1" /> 
   <meta http-equiv= "X-UA-Compatible" content= "IE=edge" />
@@ -17,25 +17,25 @@
   <link rel="stylesheet" type="text/css" href="../styles/style.css" />
  </head>
 	
-	/* Beginning of the web page's body */
+	<!-- Beginning of the web page's body -->
 	 <body>
 	 	<?php 
             include("./includes/nav.php");
     		?>
-		 /* Titre */
+		 <!-- Titre */
 		<h1> SAE 23 - Mettre en place une solution informatique pour l'entreprise </h1>
 		<br>
 		 
-		 /* Beginning of the PHP code */ 
+		<!-- Beginning of the PHP code --> 
 		<?php 
 			include ("mysql.php");
 			
-			/* Creation of the administrator account */
+			<!-- Creation of the administrator account -->
 			$requete = "SELECT count(*) FROM `Administration`";
 			$exec_requete = mysqli_query($id_bd,$requete);
 			$reponse = mysqli_fetch_array($exec_requete);
 			
-		 	/* Creation of the form allowing a user to login */ 
+		 	<!-- Creation of the form allowing a user to login -->
 			$count = $reponse['count(*)'];
 			if($count==0){
 				echo "<h1>Création de compte administrateur</h1>";
@@ -48,7 +48,7 @@
 			}
 		?>
 		 
-		 /* Saé's presentation paragraph */
+		<!-- Saé's presentation paragraph -->
 		<p> L'objectif de ce site web est d'offrir aux gestionnaires des bâtiments de l'IUT de Blagnac
 			une interface conviviale et simple pour la visualisation des données capteurs. </p>
 		<br>
@@ -56,7 +56,7 @@
 		<p> Voici la liste des bâtiments gérés avec leurs salles, où on retroue des capteurs de luminosité,
 		    CO2 et température: </p>
 		 
-		 /* Listing the building halls where there's a sensor */ 
+		<!-- Listing the building halls where there's a sensor --> 
 		<ul>
 			<li> Bâtiment B </li>
 				<ul>
@@ -72,7 +72,7 @@
 		</ul>
 		<br>
 		 
-	/* Web page's footer */
+	 <!-- Web page's footer -->
 	 <footer>
 		<ul>
 			<li><a href="mlegales.html"> Mentions Légales </a></li>

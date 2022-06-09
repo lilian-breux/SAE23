bcb1e00578f371993f53a36f372ceda611c2f010
IUTLuisGP <105793515+IUTLuisGP@users.noreply.github.com>
2022-06-09 09:40:12 +0200

    Update index.php

--- Site_web/index.php
+++ Site_web/index.php
@@ -13,7 +13,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" /> 
   <meta http-equiv= "X-UA-Compatible" content= "IE=edge" />
   <meta name="description" content="SAE" />
-  <meta name="keywords" content="HTML, CSS" />
+  <meta name="keywords" content="HTML, CSS, PHP" />
   <link rel="stylesheet" type="text/css" href="../styles/style.css" />
  </head>
 	

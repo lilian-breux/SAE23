35a7c825d54c5ba0e332463238d7c267b436a070
lilian-breux <105793062+lilian-breux@users.noreply.github.com>
2022-06-18 10:20:54 +0200

    Commantaire des codes

--- Publication/capteur.sh
+++ Publication/capteur.sh
@@ -4,19 +4,19 @@
 #Publication of data
 #
 #__________________________________________________#
-sleep 1
+sleep 1 #Wait a second to avoid data sending problems 
 if [ $3 == temperature ]
 then
-	temp=$(( $RANDOM % 11 + 19 ))
-	mosquitto_pub -h localhost -p 1883 -m "{\"bate\":\"$1\",\"room\":\"$2\",\"type\":\"$3\",\"value\":$temp,\"date\":\"$(date +"%Y-%m-%d")\",\"hours\":\"$(date +%T)\"}" -t iut/$1/$2/$3
+	temp=$(( $RANDOM % 11 + 19 )) #Random number between 19 and 30
+	mosquitto_pub -h localhost -p 1883 -m "{\"bate\":\"$1\",\"room\":\"$2\",\"type\":\"$3\",\"value\":$temp,\"date\":\"$(date +"%Y-%m-%d")\",\"hours\":\"$(date +%T)\"}" -t iut/$1/$2/$3 #Send data in a json format
 elif [ $3 == luminosite ]
 then
-	co2=$(( $RANDOM % 200 + 600 ))
-	mosquitto_pub -h localhost -p 1883 -m "{\"bate\":\"$1\",\"room\":\"$2\",\"type\":\"$3\",\"value\":$co2,\"date\":\"$(date +"%Y-%m-%d")\",\"hours\":\"$(date +%T)\"}" -t iut/$1/$2/$3
+	co2=$(( $RANDOM % 200 + 600 )) #Random number between 600 and 800
+	mosquitto_pub -h localhost -p 1883 -m "{\"bate\":\"$1\",\"room\":\"$2\",\"type\":\"$3\",\"value\":$co2,\"date\":\"$(date +"%Y-%m-%d")\",\"hours\":\"$(date +%T)\"}" -t iut/$1/$2/$3 #Send data in a json format
 elif [ $3 == co2 ]
 then
-	lum=$(( $RANDOM % 30 + 55 ))
-	mosquitto_pub -h localhost -p 1883 -m "{\"bate\":\"$1\",\"room\":\"$2\",\"type\":\"$3\",\"value\":$lum,\"date\":\"$(date +"%Y-%m-%d")\",\"hours\":\"$(date +%T)\"}" -t iut/$1/$2/$3
+	lum=$(( $RANDOM % 30 + 55 )) #Random number between 55 and 85
+	mosquitto_pub -h localhost -p 1883 -m "{\"bate\":\"$1\",\"room\":\"$2\",\"type\":\"$3\",\"value\":$lum,\"date\":\"$(date +"%Y-%m-%d")\",\"hours\":\"$(date +%T)\"}" -t iut/$1/$2/$3 #Send data in a json format
 else
 	:
 fi
--- Publication/pub.sh
+++ Publication/pub.sh
@@ -5,8 +5,7 @@
 #
 #__________________________________________________#
 
-sleep 1
-#Batiment R&T
+#Sensors for the building R&T
 bash /home/lilian/Bureau/SAE23/Publication/capteur.sh RT E208 temperature
 bash /home/lilian/Bureau/SAE23/Publication/capteur.sh RT E208 co2
 bash /home/lilian/Bureau/SAE23/Publication/capteur.sh RT E208 luminosite
@@ -14,7 +13,7 @@ bash /home/lilian/Bureau/SAE23/Publication/capteur.sh RT E207 temperature
 bash /home/lilian/Bureau/SAE23/Publication/capteur.sh RT E207 co2
 bash /home/lilian/Bureau/SAE23/Publication/capteur.sh RT E207 luminosite
 
-#Batiment Informatique
+#Sensors for the building INFO
 
 bash /home/lilian/Bureau/SAE23/Publication/capteur.sh INFO B208 temperature
 bash /home/lilian/Bureau/SAE23/Publication/capteur.sh INFO B208 co2

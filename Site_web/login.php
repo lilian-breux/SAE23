9b52f957f8a50c19b5216992fe18ea81b1747053
IUTLuisGP <105793515+IUTLuisGP@users.noreply.github.com>
2022-06-19 05:29:01 -0600

    Update footer

--- Site_web/login.php
+++ Site_web/login.php
@@ -66,5 +66,18 @@
                 ?>
             </form>
         </section>
+	<!-- Website's footer -->
+	 <footer>
+		 <ul>
+			<li><a href="mlegales.php"> Mentions Légales </a></li>
+			<li>
+				<a href="http://jigsaw.w3.org/css-validator/check/referer">
+					<img style="border:0;width:88px;height:31px"
+						src="http://jigsaw.w3.org/css-validator/images/vcss"
+						alt="¡CSS Validé!" /></a>
+			</li>
+			<li> IUT R&T </li>		
+		</ul>
+	 </footer>
     </body>
 </html>
